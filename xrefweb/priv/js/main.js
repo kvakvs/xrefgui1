768b7dca62aa7da0d0d9e95be068aed02c3a7097
JD Bothma <jbothma@gmail.com>
2013-11-20 23:33:26 +0100

    working basic force graph

--- xrefweb/priv/js/main.js
+++ xrefweb/priv/js/main.js
@@ -1,37 +1,60 @@
 // global namespace for external access
 xrefgui = {};
 
-var w = window.innerWidth,
-	h = window.innerHeight;
+var width = window.innerWidth,
+	height = window.innerHeight;
 
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+    
 /**
  * d3 uses node fields index, x, y, px, py, fixed, weight.
  *     and link fields source, target
  */
-var nodes = [],
-	links = [];
+var nodeData = [],
+	linkData = [];
 
-var force = d3.layout.force()
-    .nodes(nodes)
-    .links(links)
-    .size([w, h])
+var forceLayout = d3.layout.force()
+    .nodes(nodeData)
+    .links(linkData)
+    .size([width, height])
     .start();          // start on create
 
-var link = vis.selectAll("line")
-    .data(links)
-	.enter().append("line");
+var linkBindingSelection,
+    nodeBindingSelection;
 
-var node = vis.selectAll("circle")
-    .data(nodes)
-	.enter().append("circle")
-    	.attr("r", 5);
+function update() {
+	linkBindingSelection = svg.selectAll(".link")
+	    .data(linkData)
+		.enter().append("line")
+			.attr("class", "link");
+	
+	nodeBindingSelection = svg.selectAll(".node")
+	    .data(nodeData)
+		.enter().append("circle")
+	    	.attr("r", 5)
+	    	.attr("class", "node");
 
-force.on("tick", function() {
-  link.attr("x1", function(d) { return d.source.x; })
+	forceLayout.start();
+}
+
+forceLayout.on("tick", function() {
+  linkBindingSelection
+      .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("cx", function(d) { return d.x; })
+  nodeBindingSelection
+      .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
 });
+
+nody = {};
+nodo = {};
+linky = { "source":nody, "target":nodo};
+nodeData.push(nody);
+nodeData.push(nodo);
+linkData.push(linky);
+update();
